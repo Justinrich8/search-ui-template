f58b47f6900d105432a31060c6e03105c3f740e6
Justinrich8 <109759966+Justinrich8@users.noreply.github.com>
2023-04-26 10:05:15 -0400

    Update searchConfig.ts

--- src/config/searchConfig.ts
+++ src/config/searchConfig.ts
@@ -1,7 +1,12 @@
+import { SandboxEndpoints } from "@yext/search-headless-react"; //Include this important if you're using a sandbox environment
+
 export const config = {
-  apiKey: "yourApiKey",
-  experienceKey: "yourExpKey",
+  apiKey: "c1e993cb286cd238fcf4f43f57e686ff", //API Key for Experience with quotes
+  experienceKey: "us_presidents_demo",
   locale: "en",
-  experienceVersion: "STAGING/PRODUCTION",
-  businessId: yourBusinessIdWithoutQuotes,
+  experienceVersion: "STAGING", //Include either STAGING or PRODUCTION depending on the config label
+  SandboxEndpoints,
+  businessId: 3155537, // Business ID for Experience without quotes
 };
+
+
