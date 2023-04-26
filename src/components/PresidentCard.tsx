8c25fd4115b748858dc0220e221f06a7902cfc6a
Justinrich8 <109759966+Justinrich8@users.noreply.github.com>
2023-04-26 13:32:35 -0400

    Styling for pres card

--- src/components/PresidentCard.tsx
+++ src/components/PresidentCard.tsx
@@ -1,10 +1,43 @@
 import { CardProps, useCardAnalyticsCallback, useCardFeedbackCallback } from '@yext/search-ui-react';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 interface CustomRawDataType {
-  name: string,
-  c_body: string,
-  description: string,
+  c_politicalParty: string;
+  name: string;
+  c_body: string;
+  description: string;
+  c_serviceDates: string;
+}
+
+interface Result {
+  rawData: {
+    name: string;
+    c_politicalParty: string;
+    c_serviceDates: string;
+    c_body: string;
+  };
+}
+
+function ResultCard({ result }: { result: Result }): JSX.Element {
+  const [showFullBio, setShowFullBio] = useState(false);
+  const bioText = showFullBio ? result.rawData.c_body : result.rawData.c_body.slice(0, 500);
+
+  const toggleShowFullBio = () => {
+    setShowFullBio(!showFullBio);
+  };
+
+  return (
+    <div className='flex flex-col justify-between border rounded-lg mb-4 p-4 shadow-sm text-blue'>
+      <p><strong>Political Party: </strong>{result.rawData.c_politicalParty}</p>
+      <p><strong>Term:</strong> {result.rawData.c_serviceDates} </p>
+      <p>Bio: {bioText}</p>
+      {result.rawData.c_body.length > 500 && (
+        <button onClick={toggleShowFullBio}>
+          {showFullBio ? 'Show less' : 'Show more'}
+        </button>
+      )}
+    </div>
+  );
 }
 
 export function PresidentCard(props: CardProps<CustomRawDataType>): JSX.Element {
@@ -16,11 +49,6 @@ export function PresidentCard(props: CardProps<CustomRawDataType>): JSX.Element 
   }, [cardFeedbackCallback]);
 
   return (
-    <div className='flex flex-col justify-between border rounded-lg mb-4 p-4 shadow-sm'>
-      <p className='text-red-600'>President Card</p>
-      <button onClick={onClickTitle}>Name: {result.rawData.name}</button>
-      <p>Description: {result.rawData.c_body}</p>
-      <button onClick={onClick}>Feedback</button>
-    </div>
+    <ResultCard result={result} />
   );
 }
